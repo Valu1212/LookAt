48ff07c0bb63265c77a41e3c02410b8dd31e2899
Valu1212 <toledo.valentina@tecnica7.edu.ar>
2024-11-04 19:58:20 -0300

    fetch modificado

--- public/js/obtenerProductos.js
+++ public/js/obtenerProductos.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/productos')
+fetch('https://lookat.onrender.com/productos')
 .then (respuesta => respuesta.json())
 // .then (datos => console.log (datos))
 .then (datos => mostrarProductos(datos))
